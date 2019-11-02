69fa58ba36d749425a7e26455bb2ca719eca20c0
RimshaIshtiaq98 <36766647+RimshaIshtiaq98@users.noreply.github.com>
2019-11-02 22:56:30 +0500

    Initial Solution Setup

---
+++ ShopOnline/ShopOnile.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopOnile.WebUI.Startup))]
+namespace ShopOnile.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
